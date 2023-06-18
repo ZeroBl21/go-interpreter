b97fb50a875537fce70d847cc2051e6efb67539d
Darwin Perdomo <ZeroBl@hotmail.com>
2023-06-18 15:55:00 -0400

    feat(evaluator/): Add Boolean object return to evaluator

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -5,8 +5,14 @@ import (
 	"github.com/ZeroBl21/go-interpreter/object"
 )
 
+var (
+	TRUE  = &object.Boolean{Value: true}
+	FALSE = &object.Boolean{Value: false}
+)
+
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
+
 	// Statements
 	case *ast.Program:
 		return evalStatements(node.Statements)
@@ -17,6 +23,9 @@ func Eval(node ast.Node) object.Object {
   // Expressions
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
+
+	case *ast.Boolean:
+		return nativeBoolToBooleanObject(node.Value)
 	}
 
 	return nil
@@ -30,4 +39,13 @@ func evalStatements(stmts []ast.Statement) object.Object {
 	}
 
 	return result
+}
+
+// Returns an precreated instance of object.Boolean instead of creating new ones.
+func nativeBoolToBooleanObject(input bool) *object.Boolean {
+	if !input {
+		return TRUE
+	}
+
+	return FALSE
 }
