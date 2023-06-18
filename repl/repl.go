06c828a534afb0abc8f9060b69e37d41510ba390
Darwin Perdomo <ZeroBl@hotmail.com>
2023-06-18 15:31:56 -0400

    feat(repl/): Add evaluator to REPL for testing purposes

--- repl/repl.go
+++ repl/repl.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/ZeroBl21/go-interpreter/evaluator"
 	"github.com/ZeroBl21/go-interpreter/lexer"
 	"github.com/ZeroBl21/go-interpreter/parser"
 )
@@ -44,16 +45,19 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 
 	}
 }
 
 func printParserErrors(out io.Writer, errors []string) {
-  io.WriteString(out, MONKEY_FACE)
-  io.WriteString(out, "Woops! We ran into some monkey business here\n")
-  io.WriteString(out, " parser errors:\n")
+	io.WriteString(out, MONKEY_FACE)
+	io.WriteString(out, "Woops! We ran into some monkey business here\n")
+	io.WriteString(out, " parser errors:\n")
 
 	for _, msg := range errors {
 		io.WriteString(out, "\t"+msg+"\n")
