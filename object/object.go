2f7070f8014e13f37729d2c7f72b333414d9fa01
Darwin Perdomo <ZeroBl@hotmail.com>
2023-06-19 20:40:28 -0400

    fix(object/): Change return type of BOOLEAN_OBJ to RETURN_VALUE_OBJ

--- object/object.go
+++ object/object.go
@@ -41,5 +41,5 @@ type ReturnValue struct {
 	Value Object
 }
 
-func (rv *ReturnValue) Type() ObjectType { return BOOLEAN_OBJ }
+func (rv *ReturnValue) Type() ObjectType { return RETURN_VALUE_OBJ }
 func (rv *ReturnValue) Inspect() string  { return rv.Value.Inspect() }
