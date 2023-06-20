de5f34233137a9f0cdcfbcbac3386d38e396fe1e
Darwin Perdomo <ZeroBl@hotmail.com>
2023-06-19 20:41:13 -0400

    fix(evaluator/): Fix evalProgram redeclaration bug

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -69,7 +69,7 @@ func evalProgram(program *ast.Program) object.Object {
 	var result object.Object
 
 	for _, statement := range program.Statements {
-		result := Eval(statement)
+    result = Eval(statement)
 
 		if returnValue, ok := result.(*object.ReturnValue); ok {
 			return returnValue.Value
