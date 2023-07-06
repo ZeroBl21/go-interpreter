be90f64fb8a60a9cf770b737134b5a1575403ba0
Darwin Perdomo <ZeroBl@hotmail.com>
2023-07-06 15:01:44 -0400

    (compiler): Add compiler base setup

---
+++ compiler/compiler.go
@@ -0,0 +1,86 @@
+package compiler
+
+import (
+	"github.com/ZeroBl21/go-interpreter/ast"
+	"github.com/ZeroBl21/go-interpreter/code"
+	"github.com/ZeroBl21/go-interpreter/object"
+)
+
+type Compiler struct {
+	instructions code.Instructions
+	constants    []object.Object
+}
+
+// New creates a new Lexer instance.
+func New() *Compiler {
+	return &Compiler{
+		instructions: code.Instructions{},
+		constants:    []object.Object{},
+	}
+}
+
+func (c *Compiler) Compile(node ast.Node) error {
+	switch node := node.(type) {
+	case *ast.Program:
+		for _, s := range node.Statements {
+			err := c.Compile(s)
+			if err != nil {
+				return err
+			}
+		}
+
+	case *ast.ExpressionStatement:
+		err := c.Compile(node.Expression)
+		if err != nil {
+			return err
+		}
+
+	case *ast.InfixExpression:
+		if err := c.Compile(node.Left); err != nil {
+			return err
+		}
+
+		if err := c.Compile(node.Right); err != nil {
+			return err
+		}
+
+	case *ast.IntegerLiteral:
+		integer := &object.Integer{Value: node.Value}
+		c.emit(code.OpConstant, c.addConstant(integer))
+	}
+
+	return nil
+}
+
+// Bytecode contains the Instructions the compiler generated and the Constants
+// the compiler evaluated.
+func (c *Compiler) Bytecode() *Bytecode {
+	return &Bytecode{
+		Instructions: c.instructions,
+		Constants:    c.constants,
+	}
+}
+
+func (c *Compiler) emit(op code.Opcode, operands ...int) int {
+	ins := code.Make(op, operands...)
+	pos := c.addInstructions(ins)
+	return pos
+}
+
+// addConstant append the obj to the end of the compilers constants slice and
+// give it its very own identifier by returning its index in the constants slice.
+func (c *Compiler) addConstant(obj object.Object) int {
+	c.constants = append(c.constants, obj)
+	return len(c.constants) - 1
+}
+
+func (c *Compiler) addInstructions(ins []byte) int {
+	posNewInstruction := len(c.instructions)
+	c.instructions = append(c.instructions, ins...)
+	return posNewInstruction
+}
+
+type Bytecode struct {
+	Instructions code.Instructions
+	Constants    []object.Object
+}
