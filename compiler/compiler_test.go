3f29568f93637930c6ba2615dec154c0ec219a2a
Darwin Perdomo <ZeroBl@hotmail.com>
2023-07-20 14:41:24 -0400

    feat(compiler/): Add LocalBindings and scopes to the compiler

--- compiler/compiler_test.go
+++ compiler/compiler_test.go
@@ -495,39 +495,41 @@ func TestFunctions(t *testing.T) {
 	runCompilerTests(t, tests)
 }
 
-func TestFunctionsCalls(t *testing.T) {
+func TestFunctionCalls(t *testing.T) {
 	tests := []compilerTestCase{
 		{
-			input: `fn() { 24 }()`,
+			input: `fn() { 24 }();`,
 			expectedConstants: []any{
 				24,
 				[]code.Instructions{
-					code.Make(code.OpConstant, 0), // 24
+					code.Make(code.OpConstant, 0), // The literal "24"
 					code.Make(code.OpReturnValue),
 				},
 			},
-      expectedInstructions: []code.Instructions{
-        code.Make(code.OpConstant, 1), // the function
-        code.Make(code.OpCall),
-        code.Make(code.OpPop),
-      },
+			expectedInstructions: []code.Instructions{
+				code.Make(code.OpConstant, 1), // The compiled function
+				code.Make(code.OpCall),
+				code.Make(code.OpPop),
+			},
 		},
 		{
-			input: `let noArg = fn() { 24 }()`,
+			input: `
+      let noArg = fn() { 24 };
+      noArg();`,
 			expectedConstants: []any{
 				24,
 				[]code.Instructions{
-					code.Make(code.OpConstant, 0),
+					code.Make(code.OpConstant, 0), // The literal "24"
 					code.Make(code.OpReturnValue),
 				},
 			},
-      expectedInstructions: []code.Instructions{
-        code.Make(code.OpConstant, 1), // the function
-        code.Make(code.OpSetGlobal, 0),
-        code.Make(code.OpGetGlobal, 0),
-        code.Make(code.OpCall),
-        code.Make(code.OpPop),
-      },
+			expectedInstructions: []code.Instructions{
+				code.Make(code.OpConstant, 1), // The compiled function
+				code.Make(code.OpSetGlobal, 0),
+				code.Make(code.OpGetGlobal, 0),
+				code.Make(code.OpCall),
+				code.Make(code.OpPop),
+			},
 		},
 	}
 
@@ -539,6 +541,7 @@ func TestCompilerScopes(t *testing.T) {
 	if compiler.scopeIndex != 0 {
 		t.Errorf("scopeIndex wrong. got=%d, want=%d", compiler.scopeIndex, 0)
 	}
+	globalSymbolTable := compiler.symbolTable
 
 	compiler.emit(code.OpMul)
 
@@ -558,6 +561,10 @@ func TestCompilerScopes(t *testing.T) {
 	if last.Opcode != code.OpSub {
 		t.Errorf("lastInstruction.Opcode wrong. got=%d, want=%d",
 			last.Opcode, code.OpSub)
+	}
+
+	if compiler.symbolTable.Outer != globalSymbolTable {
+		t.Errorf("compiler did not enclose symbolTable")
 	}
 
 	compiler.leaveScope()
@@ -584,6 +591,77 @@ func TestCompilerScopes(t *testing.T) {
 		t.Errorf("previousInstruction.Opcode wrong. got=%d, want=%d",
 			previous.Opcode, code.OpMul)
 	}
+}
+
+func TestLetStatementScopes(t *testing.T) {
+	tests := []compilerTestCase{
+		{
+			input: `
+      let num = 55;
+      fn() { num }`,
+			expectedConstants: []any{
+				55,
+				[]code.Instructions{
+					code.Make(code.OpGetGlobal, 0),
+					code.Make(code.OpReturnValue),
+				},
+			},
+			expectedInstructions: []code.Instructions{
+				code.Make(code.OpConstant, 0),
+				code.Make(code.OpSetGlobal, 0),
+				code.Make(code.OpConstant, 1),
+				code.Make(code.OpPop),
+			},
+		},
+		{
+			input: `
+      fn() {
+        let num = 55;
+        num
+      }`,
+			expectedConstants: []any{
+				55,
+				[]code.Instructions{
+					code.Make(code.OpConstant, 0),
+					code.Make(code.OpSetLocal, 0),
+					code.Make(code.OpGetLocal, 0),
+					code.Make(code.OpReturnValue),
+				},
+			},
+			expectedInstructions: []code.Instructions{
+				code.Make(code.OpConstant, 1),
+				code.Make(code.OpPop),
+			},
+		},
+		{
+			input: `
+      fn() {
+        let a = 55;
+        let b = 77;
+        a + b
+      }`,
+			expectedConstants: []any{
+				55,
+				77,
+				[]code.Instructions{
+					code.Make(code.OpConstant, 0),
+					code.Make(code.OpSetLocal, 0),
+					code.Make(code.OpConstant, 1),
+					code.Make(code.OpSetLocal, 1),
+					code.Make(code.OpGetLocal, 0),
+					code.Make(code.OpGetLocal, 1),
+					code.Make(code.OpAdd),
+					code.Make(code.OpReturnValue),
+				},
+			},
+			expectedInstructions: []code.Instructions{
+				code.Make(code.OpConstant, 2),
+				code.Make(code.OpPop),
+			},
+		},
+	}
+
+	runCompilerTests(t, tests)
 }
 
 func runCompilerTests(t *testing.T, tests []compilerTestCase) {
